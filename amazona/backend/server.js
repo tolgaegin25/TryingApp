a61415e7e8b6aa94fe04d0bbea73b16026321888
tolgaegin25 <110179433+tolgaegin25@users.noreply.github.com>
2022-12-13 04:08:13 +0300

    13.12.2022

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -11,7 +11,7 @@ app.get('/api/products/slug/:slug', (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ messega: 'Product Not Found' });
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
--- amazona/frontend/src/screens/ProductScreen.js
+++ amazona/frontend/src/screens/ProductScreen.js
@@ -10,6 +10,9 @@ import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../util';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -38,7 +41,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
 
       //setProducts(result.data);
@@ -46,9 +49,9 @@ function ProductScreen() {
     fetchData();
   }, [slug]);
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
